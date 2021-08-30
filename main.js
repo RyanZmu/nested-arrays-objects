f0edc04fc807df172f71390331e969a7548bc73c
RyanZmu <ryanzmudka@gmail.com>
2021-08-30 11:15:52 -0400

    maze demo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let appElement = document.querySelector("#app")
+let appEle = document.querySelector("#app")
 
 let arrayData = [
 ["apple","banana","coconut","durian"],
@@ -14,7 +14,7 @@ for (let outerIndex = 0; outerIndex < arrayData.length; outerIndex += 1) {
     const currentArray = arrayData[outerIndex];
     let newList = document.createElement("ol")
    
-    appElement.append(newList)
+    appEle.append(newList)
 
     for (let innerIndex = 0; innerIndex < currentArray.length; innerIndex += 1) {
         const currentElement = currentArray[innerIndex];
