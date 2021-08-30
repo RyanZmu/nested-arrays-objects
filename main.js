8810f757434be2bccc82b08cdf164f71327ffc2c
RyanZmu <ryanzmudka@gmail.com>
2021-08-30 10:17:21 -0400

    class demo

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let appElement = document.querySelector("#app")
+
+let arrayData = [
+["apple","banana","coconut","durian"],
+["wheat","rye","sourdough"],
+["carrot","tomato","peach","mango"]
+]
+
+console.log(arrayData[0])
+
+for (let outerIndex = 0; outerIndex < arrayData.length; outerIndex += 1) {
+    const currentArray = arrayData[outerIndex];
+    let newList = document.createElement("ol")
+   
+    appElement.append(newList)
+
+    for (let innerIndex = 0; innerIndex < currentArray.length; innerIndex += 1) {
+        const currentElement = currentArray[innerIndex];
+        let newItem = document.createElement("li")
+        newItem.innerText = currentElement
+        newList.append(newItem)
+    } 
+}
